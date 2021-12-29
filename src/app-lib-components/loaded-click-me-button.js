c88c6d06c46ba0148323ee1e19b3d8cf4c4b6f7a
PK <saturn.creatives.theedge@gmail.com>
2021-12-30 01:07:51 +0530

    Gitlab Pages Corrected

--- src/app-lib-components/loaded-click-me-button.js
+++ src/app-lib-components/loaded-click-me-button.js
@@ -7,7 +7,7 @@ const LoadedClickMeButtonContainer = () => {
   useEffect(() => {
     gsap.to(clickMeRef.current, { rotation: '+=360' });
   }, []);
-  return (<Button type="primary" className="clickme" shape="circle" size="large" ref={clickMeRef}>HIT IT</Button>);
+  return (<Button type="primary" className="clickme" shape="circle" size="large" ref={clickMeRef}><h3> HIT IT. </h3><br></br>3D EXPERIENCE TAKES AROUND 4 MINS TO LOAD</Button>);
 };
 
 export default LoadedClickMeButtonContainer;
