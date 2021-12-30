23889c5fd18dc1b8d733a3fe908b2b6a4a9cd52e
PK <saturn.creatives.theedge@gmail.com>
2021-12-30 11:47:02 +0530

    Gitlab Pages Corrected

--- src/app-components/Portfolio.AR.VR.js
+++ src/app-components/Portfolio.AR.VR.js
@@ -3,10 +3,12 @@ import BackgroundVideo from '../app-lib-components/background.video';
 // import EyeProjector from '../app-lib-components/loaded-eye-projector';
 import ThreeDModelPlaceHolderARVR from './ThreeD.Model.PlaceHolder';
 import BGVid from '../assets/video/AR.VR.2021.Future.VBG.mp4';
+import BGAud from '../assets/audio/AR.VR.2021.Delusion32.mp3';
 
 const PortfolioARVR = () => {
   const arvrProps = {
     videoSource: BGVid,
+    audioSource: BGAud,
   };
   return (
     <BackgroundVideo arvrProps={{ ...arvrProps }}>
